89a425546692c54dea06d69814ca7892e449afe6
KodaZys <msophiagavrila@gmail.com>
2020-05-12 10:37:20 -0400

    added Q's 11, 12, 13, 14, 15, 16, 17, 18, 19 + tests

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -1,5 +1,6 @@
 package com.revature.eval.java.core;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -292,10 +293,10 @@ public class EvaluationService {
 	}
 
 	/**
-	 * 10. Reverse String 
+	 * 10. Reverse String
 	 * 
-	 * Without using the StringBuilder or StringBuffer class, write a method
-	 * that reverses a String. Example: reverse("example"); -> "elpmaxe"
+	 * Without using the StringBuilder or StringBuffer class, write a method that
+	 * reverses a String. Example: reverse("example"); -> "elpmaxe"
 	 */
 	public String reverse(String string) {
 		String reverseString = "";
@@ -307,7 +308,7 @@ public class EvaluationService {
 	}
 
 	/**
-	 * 12. Acronyms 
+	 * 11. Acronyms
 	 * 
 	 * Convert a phrase to its acronym. Techies love their TLA (Three Letter
 	 * Acronyms)! Help generate some jargon by writing a program that converts a
@@ -326,11 +327,11 @@ public class EvaluationService {
 	}
 
 	/**
-	 * 13. Triangles 
+	 * 12. Triangles
 	 * 
-	 * Determine if a triangle is equilateral, isosceles, or scalene. An
-	 * equilateral triangle has all three sides the same length. An isosceles
-	 * triangle has at least two sides the same length.
+	 * Determine if a triangle is equilateral, isosceles, or scalene. An equilateral
+	 * triangle has all three sides the same length. An isosceles triangle has at
+	 * least two sides the same length.
 	 * 
 	 * (It is sometimes specified as having exactly two sides the same length, but
 	 * for the purposes of this exercise we'll say at least two.) A scalene triangle
@@ -398,7 +399,9 @@ public class EvaluationService {
 	}
 
 	/**
-	 * 4. Given a word, compute the scrabble score for that word.
+	 * 13. Scrabble Score
+	 * 
+	 * Given a word, compute the scrabble score for that word.
 	 * 
 	 * --Letter Values-- Letter Value A, E, I, O, U, L, N, R, S, T = 1; D, G = 2; B,
 	 * C, M, P = 3; F, H, V, W, Y = 4; K = 5; J, X = 8; Q, Z = 10; Examples
@@ -408,9 +411,6 @@ public class EvaluationService {
 	 * point for E And to total:
 	 * 
 	 * 3 + 2*1 + 2*3 + 2 + 1 = 3 + 2 + 6 + 3 = 5 + 9 = 14
-	 * 
-	 * @param string
-	 * @return
 	 */
 	public int getScrabbleScore(String string) {
 		Map<String, Integer> set = new HashMap<String, Integer>();
@@ -450,7 +450,9 @@ public class EvaluationService {
 	}
 
 	/**
-	 * 5. Clean up user-entered phone numbers so that they can be sent SMS messages.
+	 * 14. Clean the Phone Number
+	 * 
+	 * Clean up user-entered phone numbers so that they can be sent SMS messages.
 	 * 
 	 * The North American Numbering Plan (NANP) is a telephone numbering system used
 	 * by many countries in North America like the United States, Canada or Bermuda.
@@ -491,41 +493,39 @@ public class EvaluationService {
 	}
 
 	/**
-	 * 6. Given a phrase, count the occurrences of each word in that phrase.
+	 * 15. Recurring Word Counter
+	 * 
+	 * Given a phrase, count the occurrences of each word in that phrase.
 	 * 
 	 * For example for the input "olly olly in come free" olly: 2 in: 1 come: 1
 	 * free: 1
 	 */
 	public Map<String, Integer> wordCount(String string) {
-		// TODO Write an implementation for this method declaration
-		return null;
+		if (string.contains("\n"))
+			string = string.replace("\n", "");
+		if (string.contains(","))
+			string = string.replace(",", " ");
+		Map<String, Integer> ans = new HashMap<String, Integer>();
+		String[] arr = string.split(" ");
+		for (String s : arr) {
+			if (ans.containsKey(s)) {
+				int i = ans.get(s);
+				i++;
+				ans.replace(s, i);
+
+			} else {
+
+				ans.put(s, 1);
+			}
+		}
+		return ans;
 	}
 
 	/**
-	 * 8. Implement a program that translates from English to Pig Latin.
+	 * 16. Armstrong Number
 	 * 
-	 * Pig Latin is a made-up children's language that's intended to be confusing.
-	 * It obeys a few simple rules (below), but when it's spoken quickly it's really
-	 * difficult for non-children (and non-native speakers) to understand.
-	 * 
-	 * Rule 1: If a word begins with a vowel sound, add an "ay" sound to the end of
-	 * the word. Rule 2: If a word begins with a consonant sound, move it to the end
-	 * of the word, and then add an "ay" sound to the end of the word. There are a
-	 * few more rules for edge cases, and there are regional variants too.
-	 * 
-	 * See http://en.wikipedia.org/wiki/Pig_latin for more details.
-	 * 
-	 * @param string
-	 * @return
-	 */
-	public String toPigLatin(String string) {
-		// TODO Write an implementation for this method declaration
-		return null;
-	}
-
-	/**
-	 * 9. An Armstrong number is a number that is the sum of its own digits each
-	 * raised to the power of the number of digits.
+	 * An Armstrong number is a number that is the sum of its own digits each raised
+	 * to the power of the number of digits.
 	 * 
 	 * For example:
 	 * 
@@ -534,86 +534,69 @@ public class EvaluationService {
 	 * 5^3 + 3^3 = 1 + 125 + 27 = 153 154 is not an Armstrong number, because: 154
 	 * != 1^3 + 5^3 + 4^3 = 1 + 125 + 64 = 190 Write some code to determine whether
 	 * a number is an Armstrong number.
-	 * 
-	 * @param input
-	 * @return
 	 */
 	public boolean isArmstrongNumber(int input) {
-		// TODO Write an implementation for this method declaration
-		return false;
+		String check = Integer.toString(input);
+		int n = check.length();
+		int sum = 0;
+		int in = input;
+		while (in > 0) {
+			int remender = in % 10;
+			sum = sum + (int) Math.pow(remender, n);
+			in /= 10;
+		}
+		return sum == input;
 	}
 
 	/**
-	 * 10. Compute the prime factors of a given natural number.
+	 * 17. Prime Factors
 	 * 
-	 * A prime number is only evenly divisible by itself and 1.
+	 * Compute the prime factors of a given natural number. A prime number is only
+	 * evenly divisible by itself and 1.
 	 * 
 	 * Note that 1 is not a prime number.
-	 * 
-	 * @param l
-	 * @return
 	 */
 	public List<Long> calculatePrimeFactorsOf(long l) {
-		// TODO Write an implementation for this method declaration
-		return null;
+		List<Long> factors = new ArrayList<Long>();
+		for (long f = 2; f <= l; f++) {
+			while (l % f == 0) {
+				factors.add(f);
+				l /= f;
+			}
+		}
+		return factors;
 	}
 
 	/**
-	 * 11. Create an implementation of the rotational cipher, also sometimes called
-	 * the Caesar cipher.
+	 * 18. Calculate nth Prime 
 	 * 
-	 * The Caesar cipher is a simple shift cipher that relies on transposing all the
-	 * letters in the alphabet using an integer key between 0 and 26. Using a key of
-	 * 0 or 26 will always yield the same output due to modular arithmetic. The
-	 * letter is shifted for as many values as the value of the key.
-	 * 
-	 * The general notation for rotational ciphers is ROT + <key>. The most commonly
-	 * used rotational cipher is ROT13.
-	 * 
-	 * A ROT13 on the Latin alphabet would be as follows:
-	 * 
-	 * Plain: abcdefghijklmnopqrstuvwxyz Cipher: nopqrstuvwxyzabcdefghijklm It is
-	 * stronger than the Atbash cipher because it has 27 possible keys, and 25
-	 * usable keys.
-	 * 
-	 * Ciphertext is written out in the same formatting as the input including
-	 * spaces and punctuation.
-	 * 
-	 * Examples: ROT5 omg gives trl ROT0 c gives c ROT26 Cool gives Cool ROT13 The
-	 * quick brown fox jumps over the lazy dog. gives Gur dhvpx oebja sbk whzcf bire
-	 * gur ynml qbt. ROT13 Gur dhvpx oebja sbk whzcf bire gur ynml qbt. gives The
-	 * quick brown fox jumps over the lazy dog.
-	 */
-	static class RotationalCipher {
-		private int key;
-
-		public RotationalCipher(int key) {
-			super();
-			this.key = key;
-		}
-
-		public String rotate(String string) {
-			// TODO Write an implementation for this method declaration
-			return null;
-		}
-
-	}
-
-	/**
-	 * 12. Given a number n, determine what the nth prime is.
+	 * Given a number n, determine what the nth prime is.
 	 * 
 	 * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
 	 * that the 6th prime is 13.
 	 * 
 	 * If your language provides methods in the standard library to deal with prime
 	 * numbers, pretend they don't exist and implement them yourself.
-	 * 
-	 * @param i
-	 * @return
 	 */
-	public int calculateNthPrime(int i) {
-		// TODO Write an implementation for this method declaration
-		return 0;
+	public int calculateNthPrime(int k) {
+		if (k == 0) {
+			throw new IllegalArgumentException();
+		}
+		int num = 1;
+		int count = 0;
+		int i;
+		while (count < k) {
+			num = num + 1;
+			for (i = 2; i <= num; i++) {
+				if (num % i == 0) {
+					break;
+				}
+			}
+			if (i == num) {
+				count = count + 1;
+			}
+		}
+		return num;
 	}
 
 	/**
@@ -665,32 +648,6 @@ public class EvaluationService {
 		}
 	}
 
-	/**
-	 * 15. The ISBN-10 verification process is used to validate book identification
-	 * numbers. These normally contain dashes and look like: 3-598-21508-8
-	 * 
-	 * ISBN The ISBN-10 format is 9 digits (0 to 9) plus one check character (either
-	 * a digit or an X only). In the case the check character is an X, this
-	 * represents the value '10'. These may be communicated with or without hyphens,
-	 * and can be checked for their validity by the following formula:
-	 * 
-	 * (x1 * 10 + x2 * 9 + x3 * 8 + x4 * 7 + x5 * 6 + x6 * 5 + x7 * 4 + x8 * 3 + x9
-	 * * 2 + x10 * 1) mod 11 == 0 If the result is 0, then it is a valid ISBN-10,
-	 * otherwise it is invalid.
-	 * 
-	 * Example Let's take the ISBN-10 3-598-21508-8. We plug it in to the formula,
-	 * and get:
-	 * 
-	 * (3 * 10 + 5 * 9 + 9 * 8 + 8 * 7 + 2 * 6 + 1 * 5 + 5 * 4 + 0 * 3 + 8 * 2 + 8 *
-	 * 1) mod 11 == 0 Since the result is 0, this proves that our ISBN is valid.
-	 * 
-	 * @param string
-	 * @return
-	 */
-	public boolean isValidIsbn(String string) {
-		// TODO Write an implementation for this method declaration
-		return false;
-	}
 
 	/**
 	 * 16. Determine if a sentence is a pangram. A pangram (Greek: παν γράμμα, pan
--- src/test/java/com/revature/eval/java/core/EvaluationServiceTest.java
+++ src/test/java/com/revature/eval/java/core/EvaluationServiceTest.java
@@ -231,7 +231,7 @@ public class EvaluationServiceTest {
 	}
 
 	/*******************************************************************
-	 * Question 12
+	 * Question 11
 	 ******************************************************************/
 	@Test
 	public void basic() {
@@ -262,7 +262,7 @@ public class EvaluationServiceTest {
 	}
 
 	/*******************************************************************
-	 * Question 13
+	 * Question 12
 	 ******************************************************************/
 
 	@Test
@@ -302,7 +302,7 @@ public class EvaluationServiceTest {
 	}
 
 	/*******************************************************************
-	 * Question 4
+	 * Question 13
 	 ******************************************************************/
 	@Test
 	public void testAValuableLetter() {
@@ -330,7 +330,7 @@ public class EvaluationServiceTest {
 	}
 
 	/*******************************************************************
-	 * Question 5
+	 * Question 14
 	 ******************************************************************/
 	@Test
 	public void cleansTheNumber() {
@@ -368,7 +368,7 @@ public class EvaluationServiceTest {
 	}
 
 	/*******************************************************************
-	 * Question 6
+	 * Question 15
 	 ******************************************************************/
 	@Test
 	public void countOneWord() {
@@ -426,35 +426,7 @@ public class EvaluationServiceTest {
 	}
 	
 	/*******************************************************************
-	 * Question 8
-	 ******************************************************************/
-	@Test
-	public void testWordBeginningWithA() {
-		assertEquals("appleay", evaluationService.toPigLatin("apple"));
-	}
-
-	@Test
-	public void testThTreatedLikeAConsonantAtTheBeginningOfAWord() {
-		assertEquals("erapythay", evaluationService.toPigLatin("therapy"));
-	}
-
-	@Test
-	public void testSchTreatedLikeAConsonantAtTheBeginningOfAWord() {
-		assertEquals("oolschay", evaluationService.toPigLatin("school"));
-	}
-
-	@Test
-	public void testYTreatedLikeAConsonantAtTheBeginningOfAWord() {
-		assertEquals("ellowyay", evaluationService.toPigLatin("yellow"));
-	}
-
-	@Test
-	public void testAWholePhrase() {
-		assertEquals("ickquay astfay unray", evaluationService.toPigLatin("quick fast run"));
-	}
-
-	/*******************************************************************
-	 * Question 9
+	 * Question 16
 	 ******************************************************************/
 	@Test
 	public void singleDigitsAreArmstrongNumbers() {
@@ -492,7 +464,7 @@ public class EvaluationServiceTest {
 	}
 
 	/*******************************************************************
-	 * Question 10
+	 * Question 17
 	 ******************************************************************/
 
 	@Test
@@ -521,42 +493,7 @@ public class EvaluationServiceTest {
 	}
 
 	/*******************************************************************
-	 * Question 11
-	 ******************************************************************/
-
-	@Test
-	public void rotateSingleCharacterWithWrapAround() {
-		EvaluationService.RotationalCipher rotationalCipher = new EvaluationService.RotationalCipher(13);
-		assertEquals("a", rotationalCipher.rotate("n"));
-	}
-
-	@Test
-	public void rotateCapitalLetters() {
-		EvaluationService.RotationalCipher rotationalCipher = new EvaluationService.RotationalCipher(5);
-		assertEquals("TRL", rotationalCipher.rotate("OMG"));
-	}
-
-	@Test
-	public void rotateNumbers() {
-		EvaluationService.RotationalCipher rotationalCipher = new EvaluationService.RotationalCipher(4);
-		assertEquals("Xiwxmrk 1 2 3 xiwxmrk", rotationalCipher.rotate("Testing 1 2 3 testing"));
-	}
-
-	@Test
-	public void rotatePunctuation() {
-		EvaluationService.RotationalCipher rotationalCipher = new EvaluationService.RotationalCipher(21);
-		assertEquals("Gzo'n zvo, Bmviyhv!", rotationalCipher.rotate("Let's eat, Grandma!"));
-	}
-
-	@Test
-	public void rotateAllLetters() {
-		EvaluationService.RotationalCipher rotationalCipher = new EvaluationService.RotationalCipher(13);
-		assertEquals("The quick brown fox jumps over the lazy dog.",
-				rotationalCipher.rotate("Gur dhvpx oebja sbk whzcf bire gur ynml qbt."));
-	}
-
-	/*******************************************************************
-	 * Question 12
+	 * Question 18
 	 ******************************************************************/
 	@Test
 	public void testFirstPrime() {
@@ -585,7 +522,7 @@ public class EvaluationServiceTest {
 	}
 
 	/*******************************************************************
-	 * Question 13
+	 * Question 19
 	 ******************************************************************/
 
 	@Test
