c15495e82b0d4e1725ca29b326512d3493ceac04
unknown <moberlies1337@gmail.com>
2020-05-13 12:50:30 -0700

    Simplified solution #3 by removing else

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -103,14 +103,13 @@ public class EvaluationService {
 	 * If the hourOfDay parameter is less than 0 or greater than 23, return false.
 	 */
 	public boolean shouldWakeUp(boolean isBarking, int hourOfDay) {
-
 		if (hourOfDay < 0 || hourOfDay > 23) {
 			return false;
 		} else if (isBarking && (hourOfDay < 8 || hourOfDay > 22)) {
 			return true;
-		} else {
-			return false;
 		}
+
+		return false;
 	}
 
 	/**
