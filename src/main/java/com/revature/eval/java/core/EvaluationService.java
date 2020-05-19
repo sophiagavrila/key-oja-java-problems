3217f9ad46c291ce55bc59667875f40d5eafb28a
KodaZys <msophiagavrila@gmail.com>
2020-05-19 16:35:43 -0400

    added Q 22

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -645,4 +645,19 @@ public class EvaluationService {
 		
 		return arr;
 	}
+	
+	/*
+	 * 22. Easy Guessing Game
+	 * 
+	 * Create a program to generate a number between the given range:
+	 * int x = minimum
+	 * iny y = maximum (inclusive)
+	 * 
+	 * You must use the Math.random class to generate a random number between x and y.
+	 */
+	
+	public int guessingGame(int x, int y) {
+		return 0;
+	}
+
 }
