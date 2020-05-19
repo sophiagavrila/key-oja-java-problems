c58fc359adc6ea5faaccc82e62a83385c3a908c4
KodaZys <msophiagavrila@gmail.com>
2020-05-19 16:24:50 -0400

    fixed 21 typo

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -627,7 +627,7 @@ public class EvaluationService {
 	 * 21. Three Magic Numbers
 	 * 
 	 * You work at a casino in Las Vegas.  Your job is to program a slot machine to
-	 * return 3 random numbers using the Math.random class.
+	 * return 3 random numbers using the java.util.Random class.
 	 * 
 	 * Write a method to return an int array of 3 random numbers between 1 - 100.
 	 * Generate the 3 random numbers (1 - 100 inclusive) using the java.util.Random class.
